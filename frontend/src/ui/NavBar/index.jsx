577f2a0b6a29403b41e3ef73d25d465cb136962d
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-03-19 08:33:14 +0100

    logo

--- frontend/src/ui/NavBar/index.jsx
+++ frontend/src/ui/NavBar/index.jsx
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom';
 import { MenuBar } from '../../components/MenuBar';
 import Button from '../../components/Button'
+import { Search } from '../../components/Search';
+import { Mark } from '../../components/Mark';
+import { LogoFilm } from '../../components/logofilm';
+import { LogoIn } from '../../components/Logoin';
 
 
 export default function NavBar() {
 
   return (
     <>
-      <nav className="bg-background">
-        <MenuBar ></MenuBar>
+      <nav className="bg-background ">
+        <ul className="flex">
+          <li className="flex">
+            <MenuBar className="w-10"></MenuBar>
+            <LogoFilm></LogoFilm>
+            <LogoIn></LogoIn>
+          </li>
+          <li><Search className="w-10" ></Search></li>
+        </ul>
       </nav>
     </>
   );
