b153620a487cd4ffce25572c58fb84c4e6ee401f
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-03-26 16:39:26 +0100

    header

--- frontend/src/ui/Header/Header.jsx
+++ frontend/src/ui/Header/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
                 <h2 className="z-10 text-forground text-center items-center justify-center font-semibold text-xl">Avec notre abonnement, accédez à des chefs-d’œuvre, des classiques,<br></br> du cinéma indépendant, des séries exclusives, des documentaires et bien plus encore.</h2>
                 <div className="z-10 flex items-center flex-col gap-3">
                     <a className="text-forground text-center items-center justify-center font-semibold text-lg">Pour commencer votre abonnement, entrez votre email ici</a>
-                    <input className="bg-background border-2 border-solid rounded-md border-forground/50 max-w-80 w-full h-10 text-forground checked:border-button"></input>
+                    <input className="bg-background p-2 border-2 border-solid rounded-md border-forground/50 max-w-80 w-full h-10 text-forground focus:border-button outline-none" placeholder="Entrer email"></input>
                     <Button>Comenzar</Button>
                     <a className="text-forground/50 text-center items-center justify-center font-semibold">Plus de 10.000 films et séries. Annulez quand vous voulez.</a>
                 </div>
