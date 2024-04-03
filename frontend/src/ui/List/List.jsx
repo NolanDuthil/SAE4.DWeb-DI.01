2bbeb6928c5e5b3dc137835ed678c52ec5f96de4
NolanDuthil <144225548+NolanDuthil@users.noreply.github.com>
2024-04-03 14:09:19 +0200

    list

--- frontend/src/ui/List/List.jsx
+++ frontend/src/ui/List/List.jsx
@@ -1,15 +1,18 @@
 import Card from "../Card/Card";
-
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 export default function List({data}) {
     return (
         <div className="mt-10 mx-12">
             <h1 className="text-forground text-2xl font-bold">Film</h1>
-            <div className=" mt-5 flex flex-row gap-5">
+            <Carousel className="mt-5">
                 {data.map((movie, index) => (
-                    <Card key={index} data={movie}/>
+                    <div key={index}>
+                        <Card data={movie}/>
+                    </div>
                 ))}
-            </div>
+            </Carousel>
         </div>
     );
 }
